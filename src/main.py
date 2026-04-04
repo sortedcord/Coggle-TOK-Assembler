147b96205142184e7822785a6ae517b09869621d
Aditya Gupta <mail@adityagupta.dev>
2026-04-04 16:56:05 +0530

    fixed time tagger

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ class TokenClass(Enum):
     ALIAS = "ALIAS"
     TIME = "TIME"
     ENUM = "ENUM"
-    LITERAL = "LITERAL"
+    LITERAL = "LITERAL"  # >> Fallback
     GRAMMAR = "GRAMMAR"
 
 
@@ -34,19 +34,21 @@ class TokenClass(Enum):
 # grammar -> spaCY
 
 
-def token_loop(span: str):
-    for token in span.split():
-        print(token)
-        token_class: TokenClass | None = None
+def token_loop(span_list: list[tuple[str, TokenClass]]) -> list[tuple[str, TokenClass]]:
+    res_span_list: list[tuple[str, TokenClass]] = []
+    for token, class_ in span_list:
+        if class_ is not TokenClass.LITERAL:
+            res_span_list.append((token, TokenClass.TIME))
+            continue
 
         mime_result = fuzzy_match_mimetype_category(token)
 
         if mime_result is not None:
-            token_class = TokenClass.MIME
+            res_span_list.append((token, TokenClass.MIME))
+        else:
+            res_span_list.append((token, TokenClass.LITERAL))
 
-        # check specific extension
-
-        print(token_class)
+    return res_span_list
 
 
 # Have some ml model determine whether its the main thing or just a modifier for the main thing
@@ -54,26 +56,34 @@ def token_loop(span: str):
 
 
 def main():
-    span = input("Enter span: ")
+    query = "get all video and audio files from last week and delete them"
 
     # query wide preprocessors
     #
-    tag_time_result = tag_time_tokens(span)
-    time_phrases: list[str] = []
+    tag_time_result = tag_time_tokens(query)
+    time_phrase: str = ""
     continue_time_phrase: bool = False
+    span_list = []
     for token in tag_time_result:
         if token.label is not None:
             if continue_time_phrase:
-                time_phrases[-1] = time_phrases[-1] + " " + token.text
+                time_phrase = time_phrase + " " + token.text
             else:
-                time_phrases.append(token.text)
+                time_phrase = token.text
 
             continue_time_phrase = True
         else:
+            if time_phrase != "":
+                span_list.append((time_phrase, TokenClass.TIME))
+                time_phrase = ""
+
+            span_list.append((token.text, TokenClass.LITERAL))
             continue_time_phrase = False
 
-    print("Time phrases:", time_phrases)
-    token_loop(span)
+    span_list = token_loop(span_list)
+
+    for token in span_list:
+        print(f"{token[0]} - {token[1]}")
 
 
 if __name__ == "__main__":
